bd0cf5a8660ed1f0bcb929930def64ff3e9aeab0
Robin Gustafsson <robin.gustafsson14@gmail.com>
2020-11-02 08:38:41 +0100

    Added comments to Token.hpp

--- include/calceval/Token.hpp
+++ include/calceval/Token.hpp
@@ -15,12 +15,21 @@
 
 namespace CalcEval
 {
+    /** Location struct implementation.
+
+        line and column numbers.
+    */
     struct Location
     {
         std::size_t line;
         std::size_t column;
     };
 
+    /** TokenType enum class implementation.
+
+        Types that the Token can be and what is
+        stored in the token value.
+    */
     enum class TokenType : uint8_t
     {
         Bad,
@@ -37,13 +46,35 @@ namespace CalcEval
         EndOfLine
     };
 
+    /** Token class implementation.
+
+        Types that the Token can be and what is
+        stored in the token value.
+    */
     class Token
     {
     public:
-        Token() = delete;
-        Token(TokenType type, const Location& loc, const std::string& value);
-        ~Token() = default;
+        /** Default Token constructor is disabled.
 
+            For now, it must be initialized.
+            In the future this might change to initialize it with
+            TokenType::Bad.
+        */
+        Token() = delete;
+
+        /** Default Token constructor with type, loc and value.
+
+            @param  type    type of the token
+            @param  loc     location where it was found
+            @param  value   token value
+            @return         default initialized Token
+        */
+        Token(TokenType type, const Location& loc, const std::string& value);
+
+        /** Function for printing the token.
+
+            @param os
+        */
         void print(std::ostream& os) const;
 
         TokenType type;
@@ -51,7 +82,19 @@ namespace CalcEval
         Location location;
     };
 
+    /** Function for converting the TokenType to a string format.
+
+        @param  type    TokenType to convert
+        @return         string format of TokenType
+    */
     [[nodiscard]] constexpr std::string_view tokenStr(TokenType type);
+
+    /** operator<< overload to print the Token.
+
+        @param os       ostream to output to
+        @param token    token to print
+        @return         ostream
+    */
     std::ostream& operator<<(std::ostream& os, const Token& token);
 
 } // namespace CalcEval
